da1e9ae10b70eff159a844156042fba6d1a184e5
Beselam-Asefa <gbeselam@gmail.com>
2019-11-07 13:23:56 +0200

    new commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict'
-console.log('my app ...');
+
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+ res.send('hellow from my node server');
+});
+app.get('/demo',(req,res)=>{
+    res.send('demo');
+   });
+
+app.listen(30000);
 
