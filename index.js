2494dcc44d17b49a3ba2b20420e93614d0d2c364
Beselam-Asefa <gbeselam@gmail.com>
2019-11-07 15:41:54 +0200

    second commit

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
+	console.log("app");
  res.send('hellow from my node server');
 });
 app.get('/demo',(req,res)=>{
+	console.log("demo app");
     res.send('demo');
    });
 
