fbf72c90db066fc77219640c0e8b2484729a76e3
Beselam-Asefa <gbeselam@gmail.com>
2019-11-15 12:56:13 +0200

    second commmit

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>{
+/* app.get('/',(req,res)=>{
 	console.log("app");
  res.send('hellow from my node server');
 });
-app.get('/demo',(req,res)=>{
+ app.get('/demo',(req,res)=>{
 	console.log("demo app");
     res.send('demo');
-   });
-
+   }); 
+ */
+app.use(express.static('public'));
 app.listen(30000);
 
